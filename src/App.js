76a9a2f010a0bcf9efc23a458105a7a5a3b00d17
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2022-06-27 18:59:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./app.css";
+import "./App.css";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import { RegisterOne,RegisterTwo,RegisterThree,RegisterFour } from './pages/index';
 const App = () => {
